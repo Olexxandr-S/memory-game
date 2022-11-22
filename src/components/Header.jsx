068f2586996640af5cfb4cfefa9703414f75aaae
OlexxandrS <94759574+OlexxandrS@users.noreply.github.com>
2022-11-22 12:50:32 +0200

    add burger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,65 @@
+import { useState } from "react";
 import "./Header.sass";
 
+const themes = [
+  { theme: "sports", text: "Sports 4X3" },
+  { theme: "earth", text: "Earth 5X4" },
+  { theme: "music", text: "Music 5X4" },
+  { theme: "animals", text: "Animals 6X4" },
+];
+
 const Header = ({ shuffleCards, setTheme, theme }) => {
+  const [active, setActive] = useState("");
+  const [burger, setBurger] = useState(false);
+
+  const randomGame = () => {
+    const random = themes[Math.floor(Math.random() * themes.length)];
+    return random;
+  };
+
   return (
-    <header className={!!theme ? theme : "header"}>
-      <h1>Memory Game</h1>
-      <button
+    <header
+      className={`${!!theme ? theme : "header"} ${burger ? "burger" : ""}`}
+    >
+      <h1
         onClick={() => {
-          shuffleCards();
-          setTheme("sports");
+          setBurger(burger === true ? false : true);
         }}
       >
-        Sports 4X3
-      </button>
+        Memory Game
+      </h1>
+      {themes.map((currentTheme, id) => {
+        return (
+          <button
+            key={id}
+            className={active === currentTheme.theme ? "active" : ""}
+            onClick={() => {
+              setBurger(false);
+              setActive(currentTheme.theme);
+              shuffleCards();
+              setTheme(`${currentTheme.theme}`);
+            }}
+          >
+            {currentTheme.text}
+          </button>
+        );
+      })}
       <button
         onClick={() => {
-          shuffleCards();
-          setTheme("earth");
+          if (theme === "") {
+            const randomTheme = randomGame().theme;
+            setBurger(false);
+            shuffleCards();
+            setActive(randomTheme);
+            setTheme(randomTheme);
+          } else {
+            setBurger(false);
+            shuffleCards();
+          }
         }}
       >
-        Earth 5X4
+        New Game
       </button>
-      <button
-        onClick={() => {
-          shuffleCards();
-          setTheme("music");
-        }}
-      >
-        Music 5X4
-      </button>
-      <button
-        onClick={() => {
-          shuffleCards();
-          setTheme("animals");
-        }}
-      >
-        Animals 6X4
-      </button>
-      <button onClick={shuffleCards}>New Game</button>
     </header>
   );
 };
